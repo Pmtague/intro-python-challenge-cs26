a5e3b1453f6265d94d19aed289d557edb1c7c1d2
Penny Tague <pmtague9@gmail.com>
2020-02-14 17:38:56 -0800

    Attempting stretch

--- src/cityreader/cityreader.py
+++ src/cityreader/cityreader.py
@@ -1,18 +1,22 @@
 # Create a class to hold a city location. Call the class "City". It should have
 # fields for name, lat and lon (representing latitude and longitude).
+
+
 class City:
-	def __init__(self, name, lat, lon):
-		self.name = name
-		self.lat = lat
-		self.lon = lon
-	
-	def __str__(self):
-	 return f'City("{self.name}", {self.lat}, {self.lon})'
+    def __init__(self, name, lat, lon):
+        self.name = name
+        self.lat = lat
+        self.lon = lon
+
+    def __str__(self):
+        return f'City("{self.name}", {self.lat}, {self.lon})'
+
 
 # We have a collection of US cities with population over 750,000 stored in the
 # file "cities.csv". (CSV stands for "comma-separated values".)
 #
-# In the body of the `cityreader` function, use Python's built-in "csv" module 
+# In the body of the `cityreader`
+#   function, use Python's built-in "csv" module
 # to read this file so that each record is imported into a City instance. Then
 # return the list with all the City instances from the function.
 # Google "python 3 csv" for references and use your Google-fu for other examples.
@@ -23,18 +27,21 @@ class City:
 # should not be loaded into a City object.
 cities = []
 
-def cityreader(cities=[]):
-	import csv
-	# TODO Implement the functionality to read from the 'cities.csv' file
-	with open('cities.csv') as f:
-		city_dict = csv.DictReader(f)
-	# For each city record, create a new City instance and add it to the 
-	# `cities` list
 
-		for row in city_dict:
-			cities.append(City(row['city'], float(row['lat']), float(row['lng'])))
-		# cities = [City(row['city'], float(row['lat']), float(row['lng'])) for row in city_dict]
-	return cities
+def cityreader(cities=[]):
+    import csv
+    # TODO Implement the functionality to read from the 'cities.csv' file
+    with open('cities.csv') as f:
+        city_dict = csv.DictReader(f)
+    # For each city record, create a new City instance and add it to the
+    # `cities` list
+
+        for row in city_dict:
+            cities.append(City(row['city'], float(
+                row['lat']), float(row['lng'])))
+        # cities = [City(row['city'], float(row['lat']), float(row['lng'])) for row in city_dict]
+    return cities
+
 
 print(cityreader(cities))
 
@@ -45,10 +52,10 @@ for c in cities:
 # STRETCH GOAL!
 #
 # Allow the user to input two points, each specified by latitude and longitude.
-# These points form the corners of a lat/lon square. Pass these latitude and 
+# These points form the corners of a lat/lon square. Pass these latitude and
 # longitude values as parameters to the `cityreader_stretch` function, along
 # with the `cities` list that holds all the City instances from the `cityreader`
-# function. This function should output all the cities that fall within the 
+# function. This function should output all the cities that fall within the
 # coordinate square.
 #
 # Be aware that the user could specify either a lower-left/upper-right pair of
@@ -73,12 +80,35 @@ for c in cities:
 
 # TODO Get latitude and longitude values from the user
 
+point1 = input("Enter lat1, lon1: ").split(",")
+# point1 = ['44.9635', '-93.2679']
+
+lat1 = float(point1[0].replace(" ", ""))
+lon1 = float(point1[1].replace(" ", ""))
+print((lat1, lon1))
+point2 = input("Enter lat2, lon2: ").split(",")
+lat2 = float(point2[0].replace(" ", ""))
+lon2 = float(point2[1].replace(" ", ""))
+
+
 def cityreader_stretch(lat1, lon1, lat2, lon2, cities=[]):
-  # within will hold the cities that fall within the specified region
-  within = []
+    # within will hold the cities that fall within the specified region
+    within = []
 
-  # TODO Ensure that the lat and lon valuse are all floats
-  # Go through each city and check to see if it falls within 
-  # the specified coordinates.
+    if lat1 < lat2:
+        newlat1 = lat2
+        newlat2 = lat1
 
-  return within
+    if lon1 < lon2:
+        newlon1 = lon2
+        newlon2 = lon1
+
+    for c in cities:
+        if lat1 <= c.lat >= lat2 and lon1 <= c.lon >= lon2:
+            within.append(c)
+
+    # TODO Ensure that the lat and lon valuse are all floats
+    # Go through each city and check to see if it falls within
+    # the specified coordinates.
+
+    return within
